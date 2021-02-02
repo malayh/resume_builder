bb17471d24b1069d546ef54c5245cdad98bd2f8a
Malay <malay.hazarika@gmail.com>
2021-02-02 23:29:51 +0530

    Adding login functionality

--- rb_frontend/src/login/Login.js
+++ rb_frontend/src/login/Login.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import $ from 'jquery';
+import {storageAvailable} from '../util'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './login.css';
@@ -7,11 +8,66 @@ import './login.css';
 export default class LoginPage extends React.Component {
     constructor(props){
         super(props);
-        this.toggle_signup = this.toggle_signup.bind(this);
+        this.toggleSignup = this.toggleSignup.bind(this);
+        this.handleLogin = this.handleLogin.bind(this);
+
+        this.state = {
+            formError:""
+        }
+
+        this.loginUrl = "http://localhost:8000/users/login/";
     }
 
-    toggle_signup() {
+
+    toggleSignup() {
         $('#login-form form').animate({height: "toggle", opacity: "toggle"}, "slow");
+        this.setState({formError:""});
+    }
+
+    validateEmail(email) {
+        if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email))
+            return true;
+        return false;
+    }
+
+    handleLoginSuccessful(data,status){
+        if(status == 404){
+            this.setState({formError:"Invalid Credentials."});
+            return;
+        }
+        else if (status == 400){
+            this.setState({formError:"Please provide both email and password."});
+            return;
+        }
+    
+        console.log(data.token);
+    }
+    handleLoginFailure(){
+        this.setState({formError:"Sorry, Something went wrong."});
+    }
+
+    handleLogin(e){
+        e.preventDefault();
+
+        if(!this.validateEmail(e.target.email.value)){
+            this.setState({formError:"Invalid Email"});
+            return;
+        }
+
+        const requestOpts = {
+            method  : "POST",
+            headers : { 'Content-Type': 'application/json' },
+            body    : JSON.stringify({ email: e.target.email.value, password: e.target.password.value })
+        };
+        
+        // fetch(this.loginUrl,requestOpts)
+        //     .then(response => response.json())
+        //     .then(data => this.handleLoginSuccessful(data), error => this.handleLoginFailure(error));
+
+        fetch(this.loginUrl,requestOpts)
+            .then((response) => { return { data : response.json(), status: response.status}; })
+            .then((resp) => {this.handleLoginSuccessful(resp.data,resp.status)}, error => this.handleLoginFailure());
+
     }
 
     render(){
@@ -19,7 +75,6 @@ export default class LoginPage extends React.Component {
             <div id="login-content">
                 <div className="container-fluid">
                     <div className="row">
-                        <div>Resume Builder</div>
                     </div>
                 </div>                
             </div>
@@ -33,14 +88,16 @@ export default class LoginPage extends React.Component {
                     <input type="text" placeholder="Email address"/>
                     <input type="password" placeholder="Password"/>
                     <input type="password" placeholder="Confirm password"/>
+                    <p className="error-msg">{this.state.formError}</p>
                     <button>Create</button>
-                    <p className="message">Already registered? <a href="#" onClick={this.toggle_signup}>Sign In</a></p>
+                    <p className="message">Already registered? <a href="#" onClick={this.toggleSignup}>Sign In</a></p>
                 </form>
-                <form className="login-form">
-                    <input type="text" placeholder="Email"/>
-                    <input type="password" placeholder="Password"/>
-                    <button>Login</button>
-                    <p className="message">Not registered? <a href="#" onClick={this.toggle_signup}>Create an account</a></p>
+                <form className="login-form" onSubmit={this.handleLogin}>
+                    <input type="text" name="email" placeholder="Email"/>
+                    <input type="password" name="password" placeholder="Password"/>
+                    <p className="error-msg">{this.state.formError}</p>
+                    <button type="submit" >Login</button>
+                    <p className="message">Not registered? <a href="#" onClick={this.toggleSignup}>Create an account</a></p>
                 </form>
                 </div>
             </div>
@@ -49,11 +106,12 @@ export default class LoginPage extends React.Component {
         const loginpage = (
             <div className="container-fluid">
                 <div className="row">
+                    <div className="col-lg-5 col-md-6 nopadding">
+                        <h1 id="app_name">Resume Builder</h1>
+                        {login_singup}
+                    </div>
                     <div className="col-lg-7 col-md-6 nopadding">
                         {content}
-                    </div>
-                    <div className="col-lg-5 col-md-6 nopadding">
-                        {login_singup}
                     </div>
                 </div>
             </div>
---
+++ rb_frontend/src/util.js
@@ -0,0 +1,31 @@
+function validateEmail(email) {
+    if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email))
+        return true;
+    return false;
+}
+
+// Rrturn true if storage available
+export function storageAvailable(type) {
+    var storage;
+    try {
+        storage = window[type];
+        var x = '__storage_test__';
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+    }
+    catch(e) {
+        return e instanceof DOMException && (
+            // everything except Firefox
+            e.code === 22 ||
+            // Firefox
+            e.code === 1014 ||
+            // test name field too, because code might not be present
+            // everything except Firefox
+            e.name === 'QuotaExceededError' ||
+            // Firefox
+            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+            // acknowledge QuotaExceededError only if there's something already stored
+            (storage && storage.length !== 0);
+    }
+}
