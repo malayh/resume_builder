8b11b9c41f6b4182f1f4675f1170194215d32049
Malay <malay.hazarika@gmail.com>
2021-02-13 22:57:25 +0530

    Adding filters in DBEndpoint

--- rb_frontend/src/common/DB.js
+++ rb_frontend/src/common/DB.js
@@ -11,15 +11,34 @@ export class DBEndpoint{
         };
 
         this.apiUrl = configs.apiHostUrl + apiUrl;
+
+        this.filter = null;
     }
+    addFilter(filter){
+        this.filter = {};
+        // filter is a dict with key value pairs, that will be converted into param
+        for(var key of Object.keys(filter)){
+            this.filter[key] = filter[key]
+        }
+    }
+    
     async createOne(dbObj){
+        
         let resp = await fetch(this.apiUrl, { headers: this.authHeaders, method:"POST", body:JSON.stringify(dbObj) });
         let data = await resp.json();
         return data;
     }
     async readAll(){
         // Return list of objs from db
-        let resp = await fetch(this.apiUrl,{ headers: this.authHeaders,method:"GET"});
+        
+        var url = new URL(this.apiUrl);
+        if(this.filter !== null){
+            for(var key of Object.keys(this.filter)){
+                url.searchParams.set(key,this.filter[key]);
+            }
+        }
+        let resp = await fetch(url,{ headers: this.authHeaders,method:"GET"});
+        this.filter = null;
         let data = await resp.json();
         return data;
     }
